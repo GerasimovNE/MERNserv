a79e6987396007c2a947a95ca3bbf2f203834946
Nikitos <nikitos@nikitos.nikitos>
2023-02-21 18:33:46 +0700

    start server with express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('huy')
+const express = require("express")
+const mongoose = require("mongoose")
+const config = require("config")
+
+
+const app = express()
+const PORT = config.get("serverPort")
+
+const start = () => {
+    try {
+        app.listen(PORT, () => {
+            console.log("zaebalo vse ", PORT)
+        })
+    }
+    catch (e) {
+        console.log(e)
+
+    }
+
+}
+start()
